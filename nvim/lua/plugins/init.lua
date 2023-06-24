43ecfeb35c08257663ab7ffdcdb4d433eaa8ecd4
kanigreg <kanigreg@gmail.com>
2023-06-24 13:38:01 +0500

    setup git plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -10,5 +10,23 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
+
+	-- Git
+	use { 'tpope/vim-fugitive' }
+	use {
+		'lewis6991/gitsigns.nvim',
+		requires = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			require('gitsigns').setup()
+		end
+	}
+	use {
+		'TimUntersberger/neogit',
+		config = function()
+			local neogit = require('neogit')
+			neogit.setup()
+		end,
+		requires = 'nvim-lua/plenary.nvim'
+	}
 end)
 
