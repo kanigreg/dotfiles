595c53d9526b33f673c1655e4e14851ae9aac20f
kanigreg <kanigreg@gmail.com>
2023-06-24 19:18:59 +0500

    setup some plugins

--- nvim/lua/plugins/.null-ls_902266_init.lua
+++ nvim/lua/plugins/.null-ls_902266_init.lua
@@ -50,8 +50,8 @@ return require('packer').startup(function(use)
 					end,
 				},
 			})
-		end
-	}
+end
+	 }
 	use {
 		'nvim-lualine/lualine.nvim',
 		requires = { 'nvim-tree/nvim-web-devicons', opt = true },
