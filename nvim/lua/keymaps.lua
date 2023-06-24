4a141995292465c95f9ef14ca2f502359dd4cc81
kanigreg <kanigreg@gmail.com>
2023-06-24 20:55:33 +0500

    add and refactor keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,11 +1,34 @@
-vim.keymap.set("n", "-", vim.cmd.Ex)
+local bind = vim.keymap.set
+local opts = { silent = true, noremap = true }
+
+bind("n", "-", vim.cmd.Ex)
+
+bind('n', '<leader>e', vim.diagnostic.open_float, opts)
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+bind('n', '<leader>b', builtin.buffers, opts)
+bind('n', '<leader>sf', builtin.find_files, opts)
+bind('n', '<leader>sg', builtin.live_grep, opts)
+bind("n", "<leader>sb", builtin.current_buffer_fuzzy_find, opts)
+bind("n", "<leader>sc", builtin.git_commits, opts)
+bind("n", "<leader>sr", builtin.lsp_references, opts)
+bind("n", "<leader>so", builtin.lsp_document_symbols, opts)
+bind("n", "<leader>sh", builtin.help_tags, opts)
+bind("n", "<leader>sd", builtin.diagnostics, opts)
 
 -- Nvim Tree
-vim.keymap.set('n', '<leader><leader>', ':NvimTreeToggle<CR>')
+bind('n', '<leader><leader>', ':NvimTreeToggle<CR>', opts)
 
+-- Core
+bind('v', '"y', '"+y', opts)
+bind('n', '"y', '"+y', opts)
+bind('n', '"Y', '"+Y', opts)
+bind('n', '"p', '"+p', opts)
+bind('n', '"P', '"+P', opts)
+
+
+bind("n", "<C-l>", "<C-w>l", opts)
+bind("n", "<C-h>", "<C-w>h", opts)
+bind("n", "<C-j>", "<C-w>j", opts)
+bind("n", "<C-k>", "<C-w>k", opts)
