fecd5a95b8bd79f6e2df8fb83af5b6c3f5784f9d
Gregory Kaniev <kanigreg@gmail.com>
2024-05-01 17:00:35 +0000

    add neotree plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,33 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not vim.loop.fs_stat(lazypath) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = " "       -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "nvim-treesitter/nvim-treesitter" },
   {
-    "folke/which-key.nvim",
-    { "folke/neoconf.nvim", cmd = "Neoconf" },
-    "folke/neodev.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
   },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  {
+    's1n7ax/nvim-window-picker',
+    name = 'window-picker',
+    event = 'VeryLazy',
+    version = '2.*',
+    config = function()
+        require'window-picker'.setup()
+    end,
+  }
 }
+
+vim.cmd.colorscheme "catppuccin"
