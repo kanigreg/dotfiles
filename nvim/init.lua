845102eefe367102de70284257df44201651c2a3
Gregory Kaniev <kanigreg@gmail.com>
2024-05-02 16:37:29 +0000

    split plugins per file

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,28 +6,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-require("lazy").setup {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "nvim-treesitter/nvim-treesitter" },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
-  {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
-    version = '2.*',
-    config = function()
-        require'window-picker'.setup()
-    end,
-  }
-}
+require("lazy").setup("plugins")
 
 vim.cmd.colorscheme "catppuccin"
