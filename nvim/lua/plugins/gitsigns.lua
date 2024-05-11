61de70e1fbfa41c4ddcc4c0c7a1d5741f30202e5
Gregory Kaniev <kanigreg@gmail.com>
2024-05-11 16:09:44 +0000

    add gitsigns

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,46 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
+      untracked = { text = '┆' },
+    },
+    on_attach = function(buffer)
+      local gs = require('gitsigns')
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      -- Navigation
+      map("n", "]h", gs.next_hunk, "Next Hunk")
+      map("n", "[h", gs.prev_hunk, "Prev Hunk")
+
+      -- Stage, unstage
+      map('n', '<leader>ghs', gs.stage_hunk, 'Stage hunk')
+      map('n', '<leader>ghr', gs.reset_hunk, 'Reset hunk')
+      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+      map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+      
+      -- Preview
+      map("n", "<leader>ghP", gs.preview_hunk, "Preview Hunk")
+      map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+
+      -- Diffs
+      map("n", "<leader>ghd", gs.diffthis, "Diff This")
+      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+
+      -- Blame
+      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+
+      -- Selection
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+    end,
+  }
+}
+
