b92bca902f40ee917bd30abf3e4270e198ffe859
kanigreg <kanigreg@gmail.com>
2024-11-10 18:33:22 +0500

    refactor: change keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,10 +4,10 @@
 
 local map = vim.keymap.set
 
-map("n", "<leader>bo", "<cmd>%bd<cr><C-O>:bd#<cr>", { desc = "Close other buffers" })
+map("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Close other buffers" })
 
 map("n", "<leader>nd", function()
   local filename = os.date("%Y/%B/%d-%a")
   local path = "~/Documents/neonotes/daily/" .. filename .. ".md"
   vim.cmd.tabnew(path)
-end)
+end, { desc = "Go to daily file" })
