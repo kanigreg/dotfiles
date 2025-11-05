b8acac174442b62acd9fcb8fc3a2d2a4d13047b7
kanigreg <kanigreg@gmail.com>
2025-11-05 17:13:57 +0500

    fix: theme linking

--- hydense/install/config/theme.sh
+++ hydense/install/config/theme.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 mkdir -p ~/.config/hydense/themes
-for f in ~/.local/share/hydense/themes/*; do ln -nfs "$f" ~/.config/hydense/themes/; done
+for src in ~/.local/share/dotfiles/hydense/themes/*; do
+  ln -snf "$src" ~/.config/hydense/themes/
+done
 
 mkdir -p ~/.config/hydense/current
 ln -snf ~/.config/hydense/themes/catppuccin ~/.config/hydense/current/theme
