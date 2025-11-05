a0cdf118f985dd0af8903142de1eca741c09465a
kanigreg <kanigreg@gmail.com>
2025-11-05 16:49:41 +0500

    fix: theme link source

--- hydense/install/config/theme.sh
+++ hydense/install/config/theme.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.config/hydense/themes
 for f in ~/.local/share/hydense/themes/*; do ln -nfs "$f" ~/.config/hydense/themes/; done
 
 mkdir -p ~/.config/hydense/current
-ln -snf "$HYDENSE_INSTALL"/themes/catppuccin ~/.config/hydense/current/theme
+ln -snf ~/.config/hydense/themes/catppuccin ~/.config/hydense/current/theme
