14bd6d5959686b9c4a26e890745ddcfe4af6ae89
kanigreg <kanigreg@gmail.com>
2025-11-05 17:03:49 +0500

    refactor: replase loaded envs

--- hydense/install/config/config.sh
+++ hydense/install/config/config.sh
@@ -2,9 +2,3 @@
 
 mkdir -p ~/.config
 cp -R "$DOTFILES_PATH"/hydense/config/* ~/.config/
-
-# TODO: Move to session manager config
-cat >~/.profile <<EOF
-HYDENSE=$DOTFILES_PATH/hydense
-PATH="$DOTFILES_PATH/hydense/bin:\$PATH"
-EOF
