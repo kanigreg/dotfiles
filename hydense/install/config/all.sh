0a014b4c126f1f013e61a49ea499fd0b0194b2bd
kanigreg <kanigreg@gmail.com>
2025-11-07 21:06:58 +0500

    fix: remove call of deleted source

--- hydense/install/config/all.sh
+++ hydense/install/config/all.sh
@@ -3,7 +3,6 @@
 source "$HYDENSE_INSTALL/config/config.sh"
 source "$HYDENSE_INSTALL/config/nvim.sh"
 source "$HYDENSE_INSTALL/config/theme.sh"
-source "$HYDENSE_INSTALL/config/firefox.sh"
 source "$HYDENSE_INSTALL/config/git.sh"
 source "$HYDENSE_INSTALL/config/docker.sh"
 source "$HYDENSE_INSTALL/config/hardware/bluetooth.sh"
