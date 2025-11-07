3fee787d79e9bd2b1a35c84ada631734904da67c
kanigreg <kanigreg@gmail.com>
2025-11-07 19:29:42 +0500

    WIP

--- hydense/install/config/theme.sh
+++ hydense/install/config/theme.sh
@@ -7,3 +7,16 @@ done
 
 mkdir -p ~/.config/hydense/current
 ln -snf ~/.config/hydense/themes/catppuccin ~/.config/hydense/current/theme
+
+# Some utils have no configurable path to the theme, so just link it in the required folder.
+mkdir -p ~/.config/btop/themes
+ln -snf ~/.config/omarchy/current/theme/btop.theme ~/.config/btop/themes/current.theme
+
+mkdir -p ~/.config/mako
+ln -snf ~/.config/omarchy/current/theme/mako.ini ~/.config/mako/config
+
+mkdir -p ~/.config/eza
+ln -snf ~/.config/omarchy/current/theme/eza.yml ~/.config/eza/theme.yml
+
+mkdir -p ~/.config/eza
+ln -snf ~/.config/omarchy/current/theme/bat.xml ~/.config/bat/themes/current.tmTheme
