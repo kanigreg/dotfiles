80cb943d1584ade59da6997e9abc483e75249076
kanigreg <kanigreg@gmail.com>
2026-02-28 13:52:40 +0500

    WIP

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 
 set -eEo pipefail
 
+SUCCESS=\033[34m%s\033[0m
+INFO=\033[32m%s\033[0m
+FAILURE=\033[31m%s\033[0m
+DEBUG=\033[90m%s\033[0m
+
 export DOTFILES="$HOME/.local/share/dotfiles"
 
 source ./install/base.sh
 
-printf "\033[32m%s\033[0m\n" "Done"
+printf "$SUCCESS\n" "Done"
